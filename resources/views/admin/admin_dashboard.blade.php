396a800d2727fc8e50268262f28d54a7f26675c8
Topu492 <topu86702@gmail.com>
2025-06-30 21:47:03 +0600

    admin templat setup update

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -1,92 +1,97 @@
 <!doctype html>
 <html lang="en">
 
-    <head>
+<head>
 
-        <meta charset="utf-8" />
-        <title>Admin Dashboard </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-        <meta content="Themesbrand" name="author" />
-        <!-- App favicon -->
-        <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.ico') }}">
+    <meta charset="utf-8" />
+    <title>Admin Dashboard </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.ico') }}">
 
-        <!-- plugin css -->
-        <link href="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css') }}" rel="stylesheet" type="text/css" />
+    <!-- plugin css -->
+    <link href="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css') }}"
+        rel="stylesheet" type="text/css" />
 
-        <!-- preloader css -->
-        <link rel="stylesheet" href="{{ asset('backend/assets/css/preloader.min.css') }}" type="text/css" />
+    <!-- preloader css -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/preloader.min.css') }}" type="text/css" />
 
-        <!-- Bootstrap Css -->
-        <link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
-        <!-- Icons Css -->
-        <link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-        <!-- App Css-->
-        <link href="{{ asset('backend/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet"
+        type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{ asset('backend/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 
-    </head>
+</head>
 
-    <body>
+<body>
 
     <!-- <body data-layout="horizontal"> -->
 
-        <!-- Begin page -->
-        <div id="layout-wrapper">
+    <!-- Begin page -->
+    <div id="layout-wrapper">
 
-            
-            @include('admin.body.header')
 
-            <!-- ========== Left Sidebar Start ========== -->
-            @include('admin.body.sidebar')
-            <!-- Left Sidebar End -->
+        @include('admin.body.header')
 
-            
+        <!-- ========== Left Sidebar Start ========== -->
+        @include('admin.body.sidebar')
+        <!-- Left Sidebar End -->
 
-            <!-- ============================================================== -->
-            <!-- Start right Content here -->
-            <!-- ============================================================== -->
-<div class="main-content">
 
-     @yield('admin')
-<!-- End Page-content -->
 
-@include('admin.body.footer')
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
 
-</div>
-<!-- end main content-->
+            @yield('admin')
+            <!-- End Page-content -->
+
+            @include('admin.body.footer')
 
         </div>
-        <!-- END layout-wrapper -->
+        <!-- end main content-->
 
-        
-        <!-- Right Sidebar -->
-        @include('admin.body.rightside')
-        <!-- /Right-bar -->
+    </div>
+    <!-- END layout-wrapper -->
 
-        <!-- Right bar overlay-->
-        <div class="rightbar-overlay"></div>
 
-        <!-- JAVASCRIPT -->
-        <script src="{{ asset('backend/assets/libs/jquery/jquery.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/metismenu/metisMenu.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/simplebar/simplebar.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/node-waves/waves.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/feather-icons/feather.min.js') }}"></script>
-        <!-- pace js -->
-        <script src="{{ asset('backend/assets/libs/pace-js/pace.min.js') }}"></script>
+    <!-- Right Sidebar -->
+    @include('admin.body.rightside')
+    <!-- /Right-bar -->
 
-        <!-- apexcharts -->
-        <script src="{{ asset('backend/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+    <!-- Right bar overlay-->
+    <div class="rightbar-overlay"></div>
 
-        <!-- Plugins js-->
-        <script src="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
-        <script src="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js') }}"></script>
-        <!-- dashboard init -->
-        <script src="{{ asset('backend/assets/js/pages/dashboard.init.js') }}"></script>
+    <!-- JAVASCRIPT -->
+    <script src="{{ asset('backend/assets/libs/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/metismenu/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/node-waves/waves.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/feather-icons/feather.min.js') }}"></script>
+    <!-- pace js -->
+    <script src="{{ asset('backend/assets/libs/pace-js/pace.min.js') }}"></script>
 
-        <script src="{{ asset('backend/assets/js/app.js') }}"></script>
+    <!-- apexcharts -->
+    <script src="{{ asset('backend/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
 
-    </body>
+    <!-- Plugins js-->
+    <script src="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js') }}">
+    </script>
+    <script
+        src="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js') }}">
+    </script>
+    <!-- dashboard init -->
+    <script src="{{ asset('backend/assets/js/pages/dashboard.init.js') }}"></script>
+
+    <script src="{{ asset('backend/assets/js/app.js') }}"></script>
+
+</body>
 
 </html>
