e54cc1d953fd23ac541c6f6dd6120526d5e65ebf
Topu492 <topu86702@gmail.com>
2025-07-08 21:19:15 +0600

    Client Cover,address,shop_info Successfully

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Models\City;
 use App\Models\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -81,40 +82,52 @@ class ClientController extends Controller
 
     public function ClientProfile()
     {
-        $id          = Auth::guard('client')->id();
+        $city = City::latest()->get();
+        $id   = Auth::guard('client')->id();
         $profileData = Client::find($id);
-        return view('client.client_profile', compact('profileData'));
+        return view('client.client_profile', compact('profileData','city'));
     }
 
     public function ClientProfileStore(Request $request)
     {
-        $id   = Auth::guard('client')->id();
+       $id = Auth::guard('client')->id();
         $data = Client::find($id);
 
-        $data->name    = $request->name;
-        $data->email   = $request->email;
-        $data->phone   = $request->phone;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
         $data->address = $request->address;
+        $data->city_id = $request->city_id;
+        $data->shop_info = $request->shop_info; 
 
         $oldPhotoPath = $data->photo;
-        if ($request->hasFile('photo')) {
-            $file     = $request->file('photo');
-            $filename = time() . '.' . $file->getClientOriginalExtension();
-            $file->move(public_path('upload/client_images'), $filename);
-            $data->photo = $filename;
 
-            if ($oldPhotoPath && $oldPhotoPath !== $filename) {
-                $this->deleteOldImage($oldPhotoPath);
-            }
+        if ($request->hasFile('photo')) {
+           $file = $request->file('photo');
+           $filename = time().'.'.$file->getClientOriginalExtension();
+           $file->move(public_path('upload/client_images'),$filename);
+           $data->photo = $filename;
+
+           if ($oldPhotoPath && $oldPhotoPath !== $filename) {
+             $this->deleteOldImage($oldPhotoPath);
+           }
+
         }
+
+        if ($request->hasFile('cover_photo')) {
+            $file1 = $request->file('cover_photo');
+            $filename1 = time().'.'.$file1->getClientOriginalExtension();
+            $file1->move(public_path('upload/client_images'),$filename1);
+            $data->cover_photo = $filename1; 
+         }
         $data->save();
-        $notification = [
-            'message'    => 'Profile Updated Successfully',
-            'alert-type' => 'success',
-        ];
+        $notification = array(
+            'message' => 'Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
         return redirect()->back()->with($notification);
     }
-    // End Method
+     // End Method 
     private function deleteOldImage(string $oldPhotoPath): void
     {
         $fullPath = public_path('upload/client_images/' . $oldPhotoPath);
