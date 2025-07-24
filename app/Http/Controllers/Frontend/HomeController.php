c86e5d2af5e04b7b71b9b9c368d79ddeca3d266f
Topu492 <topu86702@gmail.com>
2025-07-24 22:01:15 +0600

    Review All Crud Successfully

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Client;
 use App\Models\Gallery;
 use App\Models\Menu;
+use App\Models\Review;
 use App\Models\Wishlist;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -15,14 +16,37 @@ class HomeController extends Controller
 
     public function RestaurantDetails($id)
     {
-        $client = Client::find($id);
-        $menus  = Menu::where('client_id', $client->id)->get()->filter(function ($menu) {
-            return $menu->products->isNotEmpty();
-        });
-        $gallerys = Gallery::where('client_id', $id)->get();
-        return view('frontend.details_page', compact('client', 'menus', 'gallerys'));
-    }
+       $client = Client::find($id);
 
+    if (!$client) {
+    abort(404, 'Client not found');
+     }
+
+    $menus = Menu::where('client_id',$client->id)->get()->filter(function($menu){
+        return $menu->products->isNotEmpty();
+     });
+     $gallerys = Gallery::where('client_id',$id)->get();
+
+     $reviews = Review::where('client_id',$client->id)->get();
+     $totalReviews = $reviews->count();
+     $ratingSum = $reviews->sum('rating');
+     $averageRating = $totalReviews > 0 ? $ratingSum / $totalReviews : 0;
+     $roundedAverageRating = round($averageRating, 1);
+     
+     $ratingCounts = [
+        '5' => $reviews->where('rating',5)->count(),
+        '4' => $reviews->where('rating',4)->count(),
+        '3' => $reviews->where('rating',3)->count(),
+        '2' => $reviews->where('rating',2)->count(),
+        '1' => $reviews->where('rating',1)->count(),
+     ];
+     $ratingPercentages =  array_map(function ($count) use ($totalReviews){
+        return $totalReviews > 0 ? ($count / $totalReviews) * 100 : 0;
+     },$ratingCounts);
+
+     return view('frontend.details_page',compact('client','menus','gallerys','reviews','roundedAverageRating','totalReviews','ratingCounts','ratingPercentages'));
+    }
+    //End Method 
     public function AddWishList(Request $request, $id)
     {
         if (Auth::check()) {
